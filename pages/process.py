2e7225adedf204fac7882836a340bca8b56f5a85
AmyBeisel <35390179+AmyBeisel@users.noreply.github.com>
2020-03-08 16:47:33 -0600

    Add files via upload

--- pages/process.py
+++ pages/process.py
@@ -14,16 +14,17 @@ column1 = dbc.Col(
     [   
         dcc.Markdown(
             """
-            For this project I chose Olympics data because I live in Olympic City, or Colorado Springs, home of the Olympic and Paralymic
-            Committee Headquaters.  I am also an athlete myself and this area really is the perfect place for training grounds.
+            Colorado Springs is not only my home but also home to the Olympic and Paralympic Committee Headquarters. In the latest build week in my Data Science program, 
+            I chose to work with Olympic data for our machine learning project. As a professional athlete myself, Colorado Springs is perfect for training. I also have a 
+            personal passion for hearing the background of athletes' lives and love watching the games themselves.
             
             ## Process
 
-            My data exploration was fairly straight forward.  As it was fairly clean.  I decided to drop all athletes that didn't medal in the olympics. I was orginally 
-            going to try and predict which counties would have the most medals in 2020, but with where I am in ability, and talking to experts, this would have been
-            fairly difficult.  So, I decided to predict which sport one would most likely medal in based on there own stats.  
+            My data exploration was fairly straight forward.  As it was fairly clean.  I decided to drop all athletes that didn't medal in the Olympics. I was orginally 
+            going to try and predict which counties would have the most medals in 2020, but with where I am in my ability and talking to experts, this would have been
+            fairly difficult.  Therefore, I decided to predict which sport one would most likely medal in based on their own stats.  
 
-            I went down a few rabbit holes with some feature engineeing, that in the end I ended up not even using.  After cleaning up the data, mainly reducing my Sport features cardinality to 40 from 65, and getting the features I needed, I 
+            I went down a few rabbit holes with some feature engineering, that in the end I ended up not even using.  After cleaning up the data, mainly reducing my Sport features cardinality to 40 from 65, and getting the features I needed, I 
             was ready to split my data. I used a 3 way split into train, val and test.  Here is an example of my target 'Sport' based on one feature 'Age'. 
 
             """
@@ -38,9 +39,9 @@ column1 = dbc.Col(
             """
             ## Machine Learning
 
-            I did a logisic model as my majority base classifier, since this is a multi classification problem. My training accuracy was only 17%.  This means that 
+            I did a logistic model as my majority base classifier since this is a multi-classification problem. My training accuracy was only 17%.  This means that 
             without doing any machine learning on my model, it would only guess correctly 17% of the time.  For my machine learning models, I tired both Random Forest Classifer
-            and an XGboost model.  I ended up using the Random Forest as my final pipeline as it produced slightly better results.  
+            and an XGboost model.  I ended up using the Random Forest for my final pipeline as it produced slightly better results.  
             ```
             {
                 final_pipeline = make_pipeline(
@@ -59,8 +60,8 @@ column1 = dbc.Col(
             
         dcc.Markdown(
             """
-            Below are the permutation imporatances of the features. Or bagging, which is used for Random Forests.  The most imporant feature in my model was weight.  This makes sense
-            as a weightlifter, for example will be way heavier than an athete such as cycling who will be more lean. It's interesting that sex is the least
+            Below are the permutation importance of the features. Or bagging, which is used for Random Forests.  The most important feature in my model was weight.  This makes sense
+            because a weightlifter, for example, will be way heavier than an athlete such as cycling who will be leaner. It's interesting that sex is the least
             important feature.   
 
             """
@@ -73,7 +74,7 @@ column1 = dbc.Col(
              ),
         dcc.Markdown(
             """
-            Gradient Boosting, which is used for xgboost.  Below is the graph, athough it is slighly less interruptible than the 
+            Gradient Boosting, which is used for xgboost.  Below is the graph, though it is slightly less interruptible than the 
             bagging of random forest above.
 
 
@@ -89,13 +90,13 @@ column1 = dbc.Col(
             """        
             ## Results
 
-            For the Random Forest Classifer, my accuracy ending up being 42%!  Still not the best score, but it 
-            did increase from my base by 25%.  So what this means, is my model can correcly identify which sport 
-            you would most likely medal in 42% of the time in this data.  With 40 sports, or classes to choose from, I'd say this is pretty darn good. 
+            For the Random Forest Classifier, my accuracy ending up being 42%! Still not the best score, but it did increase from my base by 25%. 
+            So what this means, is my final model can now correctly identify which sport you would most likely medal in 42% of the time. With 40 sports, 
+            or classes to choose from, I'd say this is pretty darn good. 
 
-            Xgboost was only 36% accuracy, which is why I went with the Random Forest. 
+            Xgboost my model only had a 36% accuracy. 
 
-            To gain furhter insights into how the different features impacted my predictions, I created
+            To gain further insights into how the different features impacted my predictions, I created
             Shapley values on two examples:
             * A Cyclist - 
             """
@@ -117,8 +118,8 @@ column1 = dbc.Col(
             ), 
     dcc.Markdown(
             """
-            Each example interacts differenly. For the first example: season, year and weight helped the probability for the cyclist.
-            But for the Ice Hockey player, only height helped. And season, year, age and weight lowered it. 
+            Each example interacts differently. For the first example: season, year and weight helped the probability for the cyclist.
+            But for the Ice Hockey player, only height helped. And the season, year, age and weight lowered it. 
             """    
     ),
     ],
